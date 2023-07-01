8f9a8749242eda9b9685c5db572f5f90b48295da
xqkeji <xqkeji@foxmail.com>
2023-07-01 17:00:58 +0800

    安装完成后，修改相关目录的权限。

--- app/composer/Install.php
+++ app/composer/Install.php
@@ -106,6 +106,13 @@ class Install
         {
             throw new \Exception("the config directory not exists: \"$configPath\"!" , 404);
         }
+        $root_path=self::getRootPath();
+        $runtime_path=$root_path.DIRECTORY_SEPARATOR.'runtime';
+        $upload_path=$root_path.DIRECTORY_SEPARATOR.'www'.DIRECTORY_SEPARATOR.'upload';
+        $assets_path=$root_path.DIRECTORY_SEPARATOR.'www'.DIRECTORY_SEPARATOR.'assets';
+        chmod($runtime_path,0777);
+        chmod($upload_path,0777);
+        chmod($assets_path,0777);
     }
     
 }
