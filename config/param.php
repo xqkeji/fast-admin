fc62821c44dc7b99846364bf111f514f329f2d35
xqkeji <xqkeji@foxmail.com>
2023-08-06 18:06:59 +0800

    更新系统参数

--- config/param.php
+++ config/param.php
@@ -5,5 +5,5 @@ return [
 	'xq-app-mini-name'=>'新齐',
 	'xq-app-site-url'=>'https://www.xqkeji.cn/',
 	'xq-app-version'=>'1.0',
-	'xq-app-intro'=>'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;新齐通用后台管理系统是以新齐通用后台页面系统和新齐开发平台为基础开发的一套通用后台管理系统，可以帮助用户快速完成系统的开发。',
+	'xq-app-intro'=>'新齐通用后台管理系统是以新齐通用后台页面系统和新齐开发平台为基础开发的一套通用后台管理系统，可以帮助用户快速完成系统的开发。',
 ];
