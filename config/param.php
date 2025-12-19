a00a02534db52b189af78d9c26cbbd4ba8265c9c
xqkeji <xqkeji@foxmail.com>
2025-12-20 07:10:53 +0800

    更新lang文件名格式

--- config/param.php
+++ config/param.php
@@ -4,6 +4,6 @@ return [
 	'xq-app-name'=>'通用后台管理系统',
 	'xq-app-mini-name'=>'新齐',
 	'xq-app-site-url'=>'https://www.xqkeji.cn/',
-	'xq-app-version'=>'1.0',
+	'xq-app-version'=>'1.1',
 	'xq-app-intro'=>'新齐通用后台管理系统是以新齐通用后台页面系统和新齐开发平台为基础开发的一套通用后台管理系统，可以帮助用户快速完成系统的开发。',
 ];
